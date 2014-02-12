3281f52ef458ec9d28125c255330ca0da8165953
Ani <ani.ad.lib@gmail.com>
2014-02-12 11:17:35 +0200

    Add inheritance

--- Game/StopTheBunny/Tower.cs
+++ Game/StopTheBunny/Tower.cs
@@ -5,7 +5,66 @@ using System.Text;
 
 namespace StopTheBunny
 {
-    public class Tower
+    public class Tower : GameObject
     {
+        public override int PositionX
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int PositionY
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override char Sign
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override Color Color
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override char[] ElementSize
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
