5c5b48f40de63b0f08c2f9574c748b24e9732624
radochervenkov <chervenkovr@gmail.com>
2014-02-12 13:47:39 +0200

    added two files

---
+++ Game/StopTheBunny/Bunny.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopTheBunny
+{
+    class Bunny : IEnemy
+    {
+        private char symbol;
+        private Color bunnyColor;
+        private int health;
+        private int positionRow;
+        private int positionCol;
+        private int entryRow;
+        private int entryCol;
+
+        public Bunny(char symbol, Color color, int health, int entryRow, int entryCol)
+
+        public int EntryCol
+        {
+            get { return this.entryCol; }
+            set { this.entryCol = value; }
+        }
+        
+
+        public int EntryRow
+        {
+            get { return this.entryRow; }
+            set { this.entryRow = value; }
+        }
+        
+
+        public int PositionRow
+        {
+            get
+            {
+                return this.positionRow;
+            }
+            set
+            {
+                this.positionRow = value;
+            }
+        }
+
+        public int PositionCol
+        {
+            get
+            {
+                return this.positionCol;
+            }
+            set
+            {
+                this.positionCol = value;
+            }
+        }
+
+        public int Health
+        {
+            get { return this.health; }
+            set { this.health = value; }
+        }
+
+
+        public Color BunnyColor
+        {
+            get { return this.bunnyColor; }
+            set { this.bunnyColor = value; }
+        }
+
+
+        public char Symbol
+        {
+            get { return this.symbol; }
+            set { this.symbol = value; }
+        }
+
+
+        public void Move()
+        {
+            
+        }
+    }
+}
