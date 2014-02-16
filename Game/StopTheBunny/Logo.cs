89bb3b4374506b81a06ae6125085e1f2b98b1d7c
violeta <violet.rost@gamail.com>
2014-02-16 02:17:56 +0200

    менюта

--- Game/StopTheBunny/Logo.cs
+++ Game/StopTheBunny/Logo.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StopTheBunny
+﻿namespace StopTheBunny
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public static class Logo
     {
         private static int sizeMatrix = 21;
