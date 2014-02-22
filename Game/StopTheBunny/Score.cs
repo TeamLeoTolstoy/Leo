e8e84d9b5e5a8568c638e2df902c3afc9c68c908
slithize <polya.nm@gmail.com>
2014-02-22 08:50:21 +0200

    draft score

--- Game/StopTheBunny/Score.cs
+++ Game/StopTheBunny/Score.cs
@@ -7,5 +7,50 @@ namespace StopTheBunny
 {
     public class Score
     {
+        /*
+         * 
+        private const uint INITIAL_VALUE = 50; // the player can build only one tower at first.
+        private uint value;
+
+        //constructor
+        public Score(PositionOfElement position, string text, ConsoleColor color)
+        {
+            this.Position = position;
+            this.Text = text;
+            this.Color = color;
+            this.Value = INITIAL_VALUE;
+        }
+
+        // Properties
+        public uint Value
+        {
+            get;
+            set;
+        }
+        public PositionOfElement Position
+        {
+            get;
+            set;
+        }
+
+        public ConsoleColor Color
+        {
+            get;
+            set;
+        }
+
+        public string Text
+        {
+            get;
+            set;
+        }
+
+        // Method - to print the score
+        public override string ToString()
+        {
+            return string.Format("{0} -> {1}", this.Text, this.Value);
+        }
+         
+         */
     }
 }
