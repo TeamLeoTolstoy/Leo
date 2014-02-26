7d6e3ded9f3df9d386b127db80fbc5f7f2f5cb7c
radochervenkov <chervenkovr@gmail.com>
2014-02-26 21:51:31 +0200

    fixed movement

--- Game/StopTheBunny/KeyboardInterface.cs
+++ Game/StopTheBunny/KeyboardInterface.cs
@@ -1,68 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StopTheBunny
+﻿namespace StopTheBunny
 {
+    using System;
+
     public class KeyboardInterface : IUserInterface
     {
-        public void ProcessInput()
-        {
-            if (Console.KeyAvailable)
-            {
-                var keyInfo = Console.ReadKey();
-                if (keyInfo.Key.Equals(ConsoleKey.LeftArrow))
-                {
-                    if (this.OnLeftPressed != null)
-                    {
-                        this.OnLeftPressed(this, new EventArgs());
-                    }
-                }
-
-                if (keyInfo.Key.Equals(ConsoleKey.RightArrow))
-                {
-                    if (this.OnRightPressed != null)
-                    {
-                        this.OnRightPressed(this, new EventArgs());
-                    }
-                }
-
-                if (keyInfo.Key.Equals(ConsoleKey.UpArrow))
-                {
-                    if (this.OnUpPressed != null)
-                    {
-                        this.OnUpPressed(this, new EventArgs());
-                    }
-                }
-
-                if (keyInfo.Key.Equals(ConsoleKey.DownArrow))
-                {
-                    if (this.OnDownPressed != null)
-                    {
-                        this.OnDownPressed(this, new EventArgs());
-                    }
-                }
-
-                if (keyInfo.Key.Equals(ConsoleKey.Spacebar))
-                {
-                    if (this.OnBuildPressed != null)
-                    {
-                        this.OnBuildPressed(this, new EventArgs());
-                    }
-                }
-
-                if (keyInfo.Key.Equals(ConsoleKey.U))
-                {
-                    if (this.OnUpgradePressed != null)
-                    {
-                        this.OnUpgradePressed(this, new EventArgs());
-                    }
-                }                
-            }
-        }
-
         public event EventHandler OnLeftPressed;
 
         public event EventHandler OnRightPressed;
@@ -74,5 +15,65 @@ namespace StopTheBunny
         public event EventHandler OnBuildPressed;
 
         public event EventHandler OnUpgradePressed;
+
+        public void ProcessInput()
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo keyPressed = Console.ReadKey();
+                while (Console.KeyAvailable)
+                {
+                    Console.ReadKey();
+                }
+
+                if (keyPressed.Key == ConsoleKey.LeftArrow)
+                {
+                    if (this.OnLeftPressed != null)
+                    {
+                        this.OnLeftPressed(this, new EventArgs());
+                    }
+                }
+
+                if (keyPressed.Key == ConsoleKey.RightArrow)
+                {
+                    if (this.OnRightPressed != null)
+                    {
+                        this.OnRightPressed(this, new EventArgs());
+                    }
+                }
+
+                if (keyPressed.Key == ConsoleKey.UpArrow)
+                {
+                    if (this.OnUpPressed != null)
+                    {
+                        this.OnUpPressed(this, new EventArgs());
+                    }
+                }
+
+                if (keyPressed.Key == ConsoleKey.DownArrow)
+                {
+                    if (this.OnDownPressed != null)
+                    {
+                        this.OnDownPressed(this, new EventArgs());
+                    }
+                }
+
+                if (keyPressed.Key == ConsoleKey.Spacebar)
+                {
+                    if (this.OnBuildPressed != null)
+                    {
+                        this.OnBuildPressed(this, new EventArgs());
+                    }
+                }
+
+                if (keyPressed.Key == ConsoleKey.U)
+                {
+                    if (this.OnUpgradePressed != null)
+                    {
+                        this.OnUpgradePressed(this, new EventArgs());
+                    }
+                }
+            }
+        }        
     }
 }
